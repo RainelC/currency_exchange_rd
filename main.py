8bb60b782fdedcbdecc272511aae9af5c8f5b11e
Rainel Ramirez <rainel.ramirez@arajet.com>
2026-02-03 12:13:37 -0400

    feat: Implement JSON data persistence

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ def main() -> None:
     get_currency_exchange_rates_proamerica() 
     get_currency_exchange_rates_cibao() 
     get_currency_exchange_rates_reservas()
-    get_currency_exchange_rates_vimenca()    
+    # get_currency_exchange_rates_vimenca()    
     get_currency_exchange_rates_bsc() 
     get_currency_exchange_rates_caribe() 
-    get_currency_exchange_rates_popular() 
+    # get_currency_exchange_rates_popular() 
     get_currency_exchange_rates_bdi() 
     get_currency_exchange_rates_banesco() 
     show_exchange_rates()
@@ -39,41 +39,3 @@ def main() -> None:
 if __name__ == '__main__':
     main()
  
-
-
-
-
-# search = ['currency-buy-USD', 'currency-sell-USD', 'currency-buy-EUR', 'currency-sell-EUR'] #apap
-# print(scrapping(search, 'div', 'https://apap.com.do/')) 
-
-
-
-# scrapping(tag='tr',search=[], url='https://www.blh.com.do/productos/compra-y-venta-de-divisas/')
-
-
-
-# page = requests.get('https://caribeexpress.com.do/')
-
-# if page.status_code != 200:
-#     print({'status_code': page.status_code, 'message': page.content})
-
-# soup = BeautifulSoup(page.content, "html.parser")
-
-# data = soup.find_all(class_='plan plan one-third column')
-
-# # cleaning data 
-# currency={}
-# for c in data:
-#     text:str = c.text 
-#     text = text.strip().replace('$', '').replace('\r', '').strip() 
-#     text = text.split('\n')
-#     textList = [tet.strip() for tet in text if tet.strip() != '']
-#     currency[f'{textList[0]}-{textList[2]}'] = textList[1]
-
-# get_currency_exchange_rates_scrapping(data=currency, company_name='Caribe Express', param_search=['Compra'], usd_index='Dolar')
-
-# ['EUROS', '73.50', 'compra']
-# {
-#     'EUROS-compra' : 73.50,
-#     'Dolares-compra': 45.5
-# }
